76901359aa685882a12fc9524cf8c99d5c90d1d3
cml35 <claudynn@conqahq.com>
2024-07-08 23:16:05 +1200

    Add home, about and posts page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import Layout from "../components/layout";
+
+const About = () => {
+  return (
+    <main className="flex flex-col items-center justify-between pt-8">
+      <Layout>
+        <h1>About</h1>
+      </Layout>
+    </main>
+  );
+};
+
+export default About;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import Image from "next/image";
+import Layout from "../app/components/layout";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"></div>
-      <h1>Clauds website</h1>
+    <main className="flex flex-col items-center justify-between pt-8">
+      <Layout>
+        <div className="w-full flex items-center justify-center">
+          <h1>Hi, I&apos;m Claudynn</h1>🤚
+          <h2>I&apos;m a Sofware Engineer.</h2>
+        </div>
+      </Layout>
     </main>
   );
 }
