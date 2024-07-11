23308fc86e8928c8d54bb219dbe535526071da1d
cml35 <claudynn@conqahq.com>
2024-07-11 23:09:16 +1200

    Make home page nice

--- src/app/components/NavigationBar.tsx
+++ src/app/components/NavigationBar.tsx
@@ -1,120 +1,12 @@
-import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
+import { Tab, TabGroup, TabList } from "@headlessui/react";
 import Link from "next/link";
-import classNames from "classnames";
-
-import Home from "../page";
-import Posts from "../posts/page";
-import About from "../about/page";
-
-const categories = [
-  {
-    name: "🏠 Home",
-    posts: [
-      {
-        id: 1,
-        title: "Does drinking coffee make you smarter?",
-        date: "5h ago",
-        commentCount: 5,
-        shareCount: 2,
-      },
-      {
-        id: 2,
-        title: "So you've bought coffee... now what?",
-        date: "2h ago",
-        commentCount: 3,
-        shareCount: 2,
-      },
-    ],
-  },
-  {
-    name: "👩‍💻 About me",
-    posts: [
-      {
-        id: 1,
-        title: "Is tech making coffee better or worse?",
-        date: "Jan 7",
-        commentCount: 29,
-        shareCount: 16,
-      },
-      {
-        id: 2,
-        title: "The most innovative things happening in coffee",
-        date: "Mar 19",
-        commentCount: 24,
-        shareCount: 12,
-      },
-    ],
-  },
-  {
-    name: "👩‍💻 Blog Posts",
-    posts: [
-      {
-        id: 1,
-        title: "Ask Me Anything: 10 answers to your questions about coffee",
-        date: "2d ago",
-        commentCount: 9,
-        shareCount: 5,
-      },
-      {
-        id: 2,
-        title: "The worst advice we've ever heard about coffee",
-        date: "4d ago",
-        commentCount: 1,
-        shareCount: 2,
-      },
-    ],
-  },
-];
 
 const navigationItems = [
   { name: "🏠 Home", key: "home", href: "/" },
-  { name: "👩‍💻 About me", key: "about", href: "/about" },
+  { name: "🙋‍♀️ About me", key: "about", href: "/about" },
   { name: "👩‍💻 Blog Posts", key: "posts", href: "/posts" },
+  { name: "📩 Contact me", key: "contact", href: "/contact" },
 ];
-
-const renderPage = (key: string) => {
-  switch (key) {
-    case "home":
-      return (
-        <div>
-          <Link href="/dashboard">Home</Link>
-          <Home />
-        </div>
-      );
-    case "about":
-      return (
-        <div>
-          <About />
-        </div>
-      );
-    case "posts":
-      return (
-        <div>
-          <Posts />
-        </div>
-      );
-    default:
-      return (
-        <div>
-          <h1>404 Page</h1>
-          <p>This is the 404 page</p>
-        </div>
-      );
-  }
-};
-
-const getCurrentTab = (tabType: string) => {
-  switch (tabType) {
-    case "about":
-      return <About />;
-    case "posts":
-      return <Posts />;
-    case "home":
-      return <Home />;
-    default:
-      return <Home />;
-  }
-};
 
 type NavigationItem = {
   key: string;
@@ -126,7 +18,6 @@ interface TabProps {
   navigationItems: Array<NavigationItem>;
 }
 
-//data-[selected]:bg-black/5
 const Tabs = (props: TabProps) => {
   const { navigationItems } = props;
   return navigationItems.map(({ key, href, name }) => (
@@ -134,39 +25,21 @@ const Tabs = (props: TabProps) => {
       key={key}
       className="rounded-full py-1 px-3 text-sm/6 font-semibold text-black focus:outline-none data-[hover]:bg-black/5"
     >
-      {/* {({ selected }) => ( */}
-      <Link
-        //   className={classNames(
-        //     selected ? "bg-black/5" : "bg-white",
-        //     "rounded-full py-1 px-3 text-sm/6 font-semibold text-black focus:outline-none data-[hover]:bg-black/5"
-        //   )}
-        key={key}
-        href={href}
-      >
+      <Link key={key} href={href}>
         {name}
       </Link>
-      {/* )} */}
     </Tab>
   ));
 };
 
 export default function NavigationBar() {
   return (
-    <div className="flex w-full justify-center px-4">
-      <div className="w-full max-w-md">
+    <div className="flex flex-col justify-center px-4">
+      <div className="w-full">
         <TabGroup>
           <TabList className="flex gap-4">
             <Tabs navigationItems={navigationItems} />
           </TabList>
-          {/* <TabPanels className="mt-3"> */}
-          {/* {navigationItems.map(({ name, key, href }) => (
-              //   <TabPanel
-              //     key={name}
-              //     className="rounded-xl bg-black/5 p-3"
-              //   ></TabPanel>
-              
-            ))} */}
-          {/* </TabPanels> */}
         </TabGroup>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,38 @@
 import Layout from "../app/components/layout";
 
+const Intro = () => {
+  return (
+    <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="mx-auto max-w-2xl text-center">
+        <h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Bula, I&apos;m Claudynn 🤚 I&apos;m a Sofware Engineer.
+        </h2>
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          Born and raised in Fiji. I&apos;m passionate about building software
+          that is scalable, maintainable and user-friendly.
+        </p>
+        <div className="flex flex-col items-center justify-center">
+          <p className="transition duration-300 ease-in-out hover:scale-110 w-fit text-left mt-8 text-base font-semibold leading-7 text-indigo-600">
+            🏋️‍♀️ Gym Junkie
+          </p>
+          <p className="transition duration-300 ease-in-out hover:scale-110 w-fit text-left mt-1 text-base font-semibold leading-7 text-indigo-600">
+            🖋️ Amatuer Writer
+          </p>
+          <p className="transition duration-300 ease-in-out hover:scale-110 w-fit text-left mt-1 text-base font-semibold leading-7 text-indigo-600">
+            🇳🇿 Based in Auckland, NZ
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between pt-8">
       <Layout>
         <div className="w-full flex items-center justify-center">
-          <h1>Hi, I&apos;m Claudynn</h1>🤚
-          <h2>I&apos;m a Sofware Engineer.</h2>
+          <Intro />
         </div>
       </Layout>
     </main>
